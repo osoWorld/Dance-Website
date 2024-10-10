e4dfa3eeefee46fb79622d2aa71eb3a69eac5f0a
osoWorld <osoworld999@gmail.com>
2024-10-11 02:03:42 +0500

    new fixes

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use("/static", express.static("static"))    // For serving static files
 app.use(express.urlencoded());
 
 // PUG SPECIFIC STUFF
-app.set("view-engine", "pug");
+app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
 
